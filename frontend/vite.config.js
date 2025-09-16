f8465c9e5d38aca8d1bdd2330d590942817b2f06
Afrina Tithy <afrina.tithy@gmail.com>
2025-09-16 12:00:15 +0600

    Update VITE_API_URL in .env and configure server settings in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,13 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  server: {
+    host: true, // allows access from network IPs
+    port: 5173,
+    allowedHosts: [
+      'food-project.tithy.art', // add your hostname here
+      'localhost',
+      '127.0.0.1'
+    ]
+  }
 })
